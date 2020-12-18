dba25c6588d4646d80c7e375eb83685d7290523a
alielshami <elshamyali19@gmail.com>
2020-12-18 21:39:58 +0200

    add/update/delete course

--- routes/hr_routes.js
+++ routes/hr_routes.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const hrMemberModel = require("../models/hr_member_model");
 const academicMemberModel = require("../models/academic_member_model");
 const roomModel = require("../models/room_model");
-
+const courseModel = require("../models/course_model");
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -19,53 +19,6 @@ router.use((req, res, next) => {
     }
 });
 
-router.route("/update-room")
-.post(async (req,res) => {
-    let room = await roomModel.findOne({name: req.body.name});
-    if(!room) {
-        res.send("No office with such name");
-        return;
-    }
-    let persons = room.capacity-room.remainingCapacity;
-    if (persons>req.body.capacity) {
-        res.send("Cannot update capacity");
-        return;
-    }
-    if (req.body.name1) {
-        room.name = req.body.name1;
-    }
-    if (req.body.capacity) {
-        room.capacity = req.body.capacity;
-    } 
-    if (req.body.type) {
-        room.type = req.body.type;
-    }
-    room.remainingCapacity = req.body.capacity - persons;
-    try {
-        await room.save();
-        res.send("Updated room: "+room);
-    }
-    catch(error) {
-        res.send(error);
-    }
-
-})
-router.route("/delete-room")
-.post (async(req,res) => {
-    let room = await roomModel.findOne({name: req.body.name})
-    if (!room) {
-        res.send("No room to delete");
-        return;
-    }
-    try {
-        await roomModel.findOneAndDelete({name: req.body.name});
-        res.send("Deleted room: "+room);
-    }
-    catch(error)
-    {
-        res.send(error);
-    }
-}) 
 
 
 router.route("/add-hr-member")
@@ -197,5 +150,142 @@ router.route("/add-room")
         res.send(error);
     }
 })
+router.route("/update-room")
+.post(async (req,res) => {
+    let room = await roomModel.findOne({name: req.body.name});
+    let room1 = await roomModel.findOne({name: req.body.name1});
+    if(!room) {
+        res.send("No office with such name");
+        return;
+    }
+    if (room1) {
+        res.send("Name Used.. Change it");
+        return;
+    }
+    let persons = room.capacity-room.remainingCapacity;
+    if (persons>req.body.capacity) {
+        res.send("Cannot update capacity");
+        return;
+    }
+    if (req.body.name1) {
+        room.name = req.body.name1;
+    }
+    if (req.body.capacity) {
+        room.capacity = req.body.capacity;
+    } 
+    if (req.body.type) {
+        room.type = req.body.type;
+    }
+    room.remainingCapacity = req.body.capacity - persons;
+    try {
+        await room.save();
+        res.send("Updated room: "+room);
+    }
+    catch(error) {
+        res.send(error);
+    }
+
+})
+router.route("/delete-room")
+.post (async(req,res) => {
+    let room = await roomModel.findOne({name: req.body.name})
+    if (!room) {
+        res.send("No room to delete");
+        return;
+    }
+    try {
+        await roomModel.findOneAndDelete({name: req.body.name});
+        res.send("Deleted room: "+room);
+    }
+    catch(error)
+    {
+        res.send(error);
+    }
+}) 
+
+
+router.route("/add-course")
+.post(async (req,res) => {
+    const newCourse = new courseModel({
+        id: req.body.id,
+        name: req.body.name,
+        department: req.body.department,
+        instructors: req.body.instructors,
+        TAs: req.body.tas,
+        totalSlotsNumber: req.body.slots,
+        courseCoordinator: req.body.coordinator
+
+    })
+    try {
+       await newCourse.save();
+       res.send("Course Added: "+newCourse);   
+    }
+    catch (error) {
+        console.log(error.message);
+        res.send(error);
+    }
+})
+
+router.route("/update-course")
+.post(async (req,res) => {
+    let course = await courseModel.findOne({id: req.body.id});
+    let course1 = await courseModel.findOne({id: req.body.id1});
+    if (!course) {
+        res.send("No course with such ID");
+        return;
+    }
+    if (course1) {
+        res.send("ID Used.. Change it");
+        return;
+    }
+    if (req.body.id1) {
+        course.id = req.body.id1;
+    }
+    if (req.body.name) {
+        course.name = req.body.name;
+    }
+    if (req.body.department) {
+        course.department = req.body.department;
+    }
+    if (req.body.instructors) {
+        course.instructors = req.body.instructors;
+    }
+    if (req.body.tas) {
+        course.TAs = req.body.tas;
+    }
+    if (req.body.slots) {
+        course.totalSlotsNumber = req.body.slots;
+    }
+    if (req.body.coordinator) {
+        course.courseCoordinator = req.body.coordinator;
+    }
+    try {
+        await course.save();
+        res.send("Updated Course: "+course);
+    }
+    catch(error)
+    {
+        res.send(error);
+    }
+})
+
+router.route("/delete-course")
+.post (async(req,res) => {
+    let deletedCourse = await courseModel.findOne({id: req.body.id})
+    if (!deletedCourse) {
+        res.send("No course to delete");
+        return;
+    }
+    try {
+        await courseModel.findOneAndDelete({id: req.body.id});
+        res.send("Deleted course: "+deletedCourse);
+    }
+    catch(error)
+    {
+        res.send(error);
+    }
+}) 
+
+
 
 module.exports = router;
