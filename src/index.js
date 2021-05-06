2b820f237070f9ec0bb988756de99cc72216d956
Mariam <mariamashraf587@yahoo.com>
2021-05-07 00:36:30 +0200

    changed port for deployment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const { app } = require("./app");
-app.listen(process.env.PORT);
+app.listen(process.env.PORT||5000);
 
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
